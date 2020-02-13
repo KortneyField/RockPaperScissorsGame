1bbeb826c9e4c403d760cb9e7c01a8df22c9dfd0
Kortney <kfiel48@wgu.edu>
2020-02-12 21:39:16 -0600

    Working edits for invalid odd number

--- rockgame2.py
+++ rockgame2.py
@@ -12,52 +12,57 @@ def main():
     print()
     play()
 
-
+def play_amount():
+    play_num = int(input("Best out of how many rounds (must be an odd #):  "))
+    if (play_num % 2) != 0:  
+        return play_num  
+    else:  
+        play_num = input("INVALID: Must pick a odd number. ")
 
 def play():
     human = 0
     computer = 0
     
-    play_num = int(input("How many wins do you want to play to?:  "))
-
-    while (human <= play_num or computer <= play_num):
-        
+    n = play_amount()
+    wins = 0  
+    
+    while (wins < n):
         c_hand=random.choice(selections_list).upper()
         p_hand=input("Choose rock, paper or scissors: ").upper()
-
-        if play == "yes":
-            if p_hand == c_hand:
-                print("It is a Tie")
-            elif p_hand == 'ROCK':
-                if c_hand == 'PAPER':
-                    print("Paper covers rock, YOU LOST")
-                    computer += 1
-                elif c_hand == 'SCISSORS':
-                    print("Rock beats Scissors, YOU WIN")
-                    human += 1
-            elif p_hand == 'PAPER':
-                if c_hand == 'ROCK':
-                    print("Paper covers Rock, YOU WIN")
-                    human += 1
-                elif c_hand == 'SCISSORS':
-                    print("Scissors cut Paper, YOU LOST")
-                    computer += 1
-            elif p_hand == 'SCISSORS':
-                if c_hand == 'PAPER':
-                    print("Scissors cut Paper, YOU WIN")
-                    human += 1
-                elif c_hand == 'ROCK':
-                    print("Rock beats Scissors, YOU LOST")
-                    computer += 1
-            
-            print("The Score>>>")
-            print("    HUMAN = {}".format(human))
-            print("  COMPTER = {}".format(computer))
+        if: 
+            p_hand = input("INVALID- Try again:")
+        if p_hand == c_hand:
+            print("It is a Tie")
+        elif p_hand == 'ROCK':
+            if c_hand == 'PAPER':
+                print("Paper covers rock, YOU LOST")
+                computer += 1
+                wins += 1
+            elif c_hand == 'SCISSORS':
+                print("Rock beats Scissors, YOU WIN")
+                human += 1
+                wins += 1
+        elif p_hand == 'PAPER':
+            if c_hand == 'ROCK':
+                print("Paper covers Rock, YOU WIN")
+                human += 1
+                wins += 1
+            elif c_hand == 'SCISSORS':
+                print("Scissors cut Paper, YOU LOST")
+                computer += 1
+                wins += 1
+        elif p_hand == 'SCISSORS':
+            if c_hand == 'PAPER':
+                print("Scissors cut Paper, YOU WIN")
+                human += 1
+                wins += 1
+            elif c_hand == 'ROCK':
+                print("Rock beats Scissors, YOU LOST")
+                computer += 1
         
-        if play == "no":
-            play == False
-
-    else:
-        print("game over")
+        
+        print("The Score>>>")
+        print("    HUMAN = {}".format(human))
+        print("  COMPTER = {}".format(computer))
 
 main()
