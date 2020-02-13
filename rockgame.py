a90303d89265401805ca95e774f481d9c04e22d9
Kortney <kfiel48@wgu.edu>
2020-02-13 15:44:44 -0600

    score keeper and input validation

--- rockgame.py
+++ rockgame.py
@@ -1,4 +1,5 @@
 import random
+import os
 
 WIN_LIST_RPS = [('ROCK', 'SCISSORS'),
                 ('SCISSORS', 'PAPER'),
@@ -12,18 +13,23 @@ win_list = WIN_LIST_RPS
 
 computer_selection = random.choice(selections_list)
 
-def user_input():
-    user_input = input("Play your hand:  ")
-    user_input = user_input.upper()
 
-def start_game(player_hand):
+
+def start_game():
     print("*** Let's Play the Rock Paper Scissor Game ***")
     print(" ")
     print("Choose ROCK, PAPER or SCISSOR")
-    player_hand = user_input()
 
-def match (user_input, computer_selection):
-    match = user_input, computer_selection
+def user_input():
+    user_input = input("Play your hand:  ")
+    return user_input
+    #print("You are fighting with {}".format(user_input))
+
+def match ():
+    user_input=user_input()
+    computer_selection=random.choice(selections_list)
+    match = hand, computer_selection
+    print("You are {}, going against {}".format(hand,computer_selection))
 
 def competition(match):
     if user_input == computer_selection:
@@ -33,8 +39,16 @@ def competition(match):
     else:
         print("\nResult: The power of {} is stronger than {}! You lost!".format(computer_selection, user_input))
 
+def play_again():
+    play = input("Want to play again? y/n:  ")
+    if play.lower() == 'p':
+        main()
 
+def main():
+    #os.system("CLEAR")
+    start_game()
+    user_input()
+    match()
+    competition(match)
 
-
-start_game(user_input)
-competition(match)
+main()
